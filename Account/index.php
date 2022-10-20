c76a5f450bf0ef1eb17000fc02678344b61aa97f
Ukatu Ikechukwu BSSL <ukatu.bsslh@email.com>
2022-10-20 15:51:08 +0100

    Commit chnages

---
+++ Account/index.php
@@ -0,0 +1,171 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Artisan_Hub | Get Started</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="../css1/bootstrap-grid.css" />
+    <link rel="stylesheet" href="../css1/icons.css">
+    <link rel="stylesheet" href="../css1/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="../css1/style.css" />
+    <link rel="stylesheet" type="text/css" href="../css1/responsive.css" />
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../css1/chosen.css" />
+    <link rel="stylesheet" type="text/css" href="../css1/colors/colors.css" />
+    <link rel="stylesheet" type="text/css" href="../css1/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="../css1/bootstrapmaxcdn.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+    <link rel="icon" href="../Uploads/General/myhandworkng.png" type="image/x-icon">
+
+
+
+
+</head>
+<body class="newbg">
+    <div class="theme-layout" id="scrollup">
+
+        <div class="responsive-header three">
+            <div class="responsive-menubar">
+                <div class="res-logo"><a title="" href="../myhandwork_default.html"><img src="../Uploads/General/myhandworkng-logo.png" alt="" /></a></div>
+                <div class="menu-resaction">
+                    <div class="res-openmenu">
+                        <img src="../images1/icon.png" alt="" /> Menu <i class="la la-bars"></i>
+                    </div>
+                    <div class="res-closemenu">
+                        <img src="../images1/icon2.png" alt="" /> Close <i class="la la-close"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="responsive-opensec ">
+                <div class="btn-extars">
+                        <a class="post-job-btn" href="index.php"><i class="la la-registered"></i>Register now</a>
+                        <ul class="account-btns">
+                            <li><a href="Login.htm"><i class="la la-sign-in"></i> Login</a></li>
+                        </ul>
+
+                </div>
+
+                <div class="responsivemenu">
+                    <ul>
+                        <!-- <li>
+                                <a class="post-job-btn open-contact" style="background-color:#d42525; border-color:white; color:white">Post a task</a>
+                        </li>
+                        <li>
+                            <a href="../Handworks.htm">Hire handyman</a>
+                        </li>
+                        <li>
+                            <a href="../Handworks/handyJobs.htm"> Browse tasks</a>
+                        </li>
+                        <li>
+                            <a href="../Affiliates.htm">Affiliate</a>
+                        </li> -->
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <header class="stick-top forsticky new-header">
+            <div class="menu-sec">
+                <div class="container">
+                    <div class="logo">
+                        <a href="../myhandwork_default.html"><img src="../Uploads/General/myhandworkng-logo.png" alt="" /></a>
+                    </div><!-- Logo -->
+                    <div class="btn-extars">
+                            <a class="post-job-btn" href="index.php"><i class="la la-registered"></i>Register now</a>
+                            <ul class="account-btns">
+                                <li><a href="Login.htm"><i class="la la-sign-in"></i> Login</a></li>
+                            </ul>
+
+                    </div><!-- Btn Extras -->
+                    <nav>
+                        <ul>
+                            <!-- <li>
+                                    <a class="post-job-btn open-contact" style="background-color:#d42525; border-color:white; color:white">Post a task</a>
+                            </li>
+                            <li>
+                                <a href="../Handworks.htm">Hire handyman</a>
+                            </li>
+                            <li>
+                                <a href="../Handworks/handyJobs.htm"> Browse tasks</a>
+                            </li>
+                            <li>
+                                <a href="../Affiliates.htm">Affiliate</a>
+                            </li> -->
+                        </ul>
+                    </nav><!-- Menus -->
+                </div>
+            </div>
+        </header>
+
+
+
+
+
+        <!-- Main Page-Body Content -->
+        <section>
+
+            <section><br /><br /></section>
+            
+
+
+
+<section>
+    <div class="block remove-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="account-popup-area signup-popup-box static">
+                        <div class="account-popup">
+
+                            <br />
+                            <h3>Tell us about yourself</h3>
+
+<form action="index.php" method="post">                                <p><div class="btn btn-lg btn-success active"><a href="Signup.htm"> I want to Register as an Artisan</a></div></p>
+                                <p><div class="btn btn-lg btn-light active"><a style="color:green; font-weight:400" href="ClientSignUp.htm">I want to hire an Artisan for a Job</a></div></p>
+                                <br /> <br /> <br /> <br /> <br />
+<input name="__RequestVerificationToken" type="hidden" value="CfDJ8IxpwAgpJgZKhredemyMRdCMb17NohuDgv5lqEKi1J6UzwLngOAiSdaNUI-lKV5Kw8yRwIEPwJSgYxqfd7pRsqY5UpVfJQKxPgLUV8sfdMYph0eBVyZR6Jnbc2SLiSLttGiMGpzaFfL10-WtzFSqnfw" /></form>                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+        </section>
+
+
+
+        <footer class="ft">
+            <!-- <div class="bottom-line">
+                <div class="container">
+                    <span>©
+                    <script>document.write(new Date().getFullYear())</script> myhandwork.ng. All rights reserved.</span>
+                    <a href="https://myhandwork.ng/Account/index.php#scrollup" class="scrollup" title=""><i class="la la-long-arrow-up"></i></a>
+                </div>
+            </div> -->
+        </footer>
+
+    </div>
+
+    <script src="../js1/jquery.min.js" type="text/javascript"></script>
+    <script src="../js1/modernizr.js" type="text/javascript"></script>
+    <script src="../js1/script.js" type="text/javascript"></script>
+    <script src="../js1/bootstrap.min.js" type="text/javascript"></script>
+    <script src="../js1/wow.min.js" type="text/javascript"></script>
+    <script src="../js1/slick.min.js" type="text/javascript"></script>
+    <script src="../js1/parallax.js" type="text/javascript"></script>
+    <script src="../js1/select-chosen.js" type="text/javascript"></script>
+    <script src="../js1/jquery.scrollbar.min.js" type="text/javascript"></script>
+    <script src="../js1/counter.js" type="text/javascript"></script>
+</body>
+</html>
